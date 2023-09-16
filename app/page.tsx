bfb02bc678fef201d3860c856c86096caea40e4e
minho <dnjsalsgh123@gmail.com>
2023-09-16 09:33:26 +0900

    [Update]: why use cn because use variable in className

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const state = true;
 
 export default function Home() {
   return (
     <div className="">
       <p className="text-3xl font-bold text-indigo-500">gg</p>
-      <Button>Click me</Button>
+      <Button className={cn("bg-indigo-500", state && "bg-red-500")}>
+        Click me
+      </Button>
     </div>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// cn은 tailwind에서 다이나믹 class를 지원하게 해주는 역할입니다.
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
