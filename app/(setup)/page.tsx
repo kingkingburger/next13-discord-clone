1bf4c882a21e87cf4073b2e805ba6c068037ab40
minho <dnjsalsgh123@gmail.com>
2023-09-21 20:10:15 +0900

    [Update]: initial modal page

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -20,6 +21,6 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 export default SetupPage;
