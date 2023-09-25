5ce9509167211ecf6ef1c935f49dac06dd439da1
minho <dnjsalsgh123@gmail.com>
2023-09-25 21:16:18 +0900

    [Update]: make server use zustand and implement Server(Channel) add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
+
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -27,6 +28,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discordj-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
