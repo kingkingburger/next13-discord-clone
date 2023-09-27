f3bb40d5c2ba8d17c18f60ddb86ec13a569b6a12
minho <dnjsalsgh123@gmail.com>
2023-09-28 06:12:13 +0900

    [Update]: setting invite code in the modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -17,6 +17,7 @@ import {
   UserPlus,
   Users,
 } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMemberWithProfiles;
@@ -24,6 +25,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -46,7 +49,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
        dark:text-neutral-400 space-y-[2px]"
       >
         {isModerator && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server: server })}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
             Invite People
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,17 +1,23 @@
 import { create } from "zustand";
+import { Server } from "@prisma/client";
+export type ModalType = "createServer" | "invite";
 
-export type ModalType = "createServer";
+interface ModalData {
+  server?: Server;
+}
 
 interface ModalStore {
   type: ModalType | null;
+  data: ModalData;
   isOpen: boolean;
-  onOpen: (type: ModalType) => void;
+  onOpen: (type: ModalType, data?: ModalData) => void;
   onClose: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
   type: null,
   isOpen: false,
-  onOpen: (type) => set({ isOpen: true, type }),
+  data: {},
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
   onClose: () => set({ type: null, isOpen: false }),
 }));
