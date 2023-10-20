e281c57cb7a419b2aa2d7fa1b405ac0528c2ea6f
minho <dnjsalsgh123@gmail.com>
2023-10-20 23:31:18 +0900

    [Update]: initialize edit channel modal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -8,7 +8,8 @@ export type ModalType =
   | "createChannel"
   | "leaveServer"
   | "deleteServer"
-  | "deleteChannel";
+  | "deleteChannel"
+  | "editChannel";
 
 interface ModalData {
   server?: Server;
