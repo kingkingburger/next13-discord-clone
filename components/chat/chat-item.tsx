a1742ef2c2004896d724cbd2f385a11336c6db66
minho <dnjsalsgh123@gmail.com>
2023-11-12 09:28:40 +0900

    [Update]: implement chat icon

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Member, Profile } from "@prisma/client";
+import { UserAvatar } from "@/components/user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
 
 interface ChatItemProps {
   id: string;
@@ -17,6 +20,12 @@ interface ChatItemProps {
   socketQuery: Record<string, string>;
 }
 
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
+
 export const ChatItem = ({
   id,
   content,
@@ -29,5 +38,28 @@ export const ChatItem = ({
   socketUrl,
   socketQuery,
 }: ChatItemProps) => {
-  return <div>chat Itme</div>;
+  return (
+    <div className="relactive group flex items-center hover:bg-black/5 p-4 transition w-full">
+      <div className="group flex gap-x-2 items-start w-full">
+        <div className="cursor-pointer hover:drop-shadow-md transition">
+          <UserAvatar src={member.profile.imageUrl} />
+        </div>
+        <div className="flex flex-col w-full">
+          <div className="flex items-center gap-x-2">
+            <div className="flex items-center">
+              <p className="font-semibold text-sm hover:underline cursor-pointer">
+                {member.profile.name}
+              </p>
+              <ActionTooltip label={member.role}>
+                {roleIconMap[member.role]}
+              </ActionTooltip>
+            </div>
+            <span className="text-xs text-zinc-500 dark:text-zinc-400">
+              {timestamp}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
