167ea4389bc754a52702c43c0eb9e529c1c03b9d
minho <dnjsalsgh123@gmail.com>
2023-11-29 20:10:31 +0900

    [Update]: setting media room component for using livekit api

---
+++ components/media-room.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Channel } from "@prisma/client";
+import { useUser } from "@clerk/nextjs";
+
+interface MediaRoomProps {
+  chatId: string;
+  video: boolean;
+  audio: boolean;
+}
+
+export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
+  const { user } = useUser();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (!user?.firstName || !user?.lastName) return;
+
+    const name = `${user.firstName} ${user.lastName}`;
+
+    async () => {
+      try {
+        const resp = await fetch(
+          `/api/livekit?room=${chatId}&username=${name}`
+        );
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  }, [user?.firstName, user?.lastName, chatId]);
+
+  return <div> Me</div>;
+};
