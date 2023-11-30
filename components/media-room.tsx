6185fecbd920d775089d44dbbadf748fa7b14e6c
minho <dnjsalsgh123@gmail.com>
2023-11-30 21:31:15 +0900

    [Update]: implement audio, video component

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -5,6 +5,7 @@ import { LiveKitRoom, VideoConference } from "@livekit/components-react";
 import "@livekit/components-styles";
 import { Channel } from "@prisma/client";
 import { useUser } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 interface MediaRoomProps {
   chatId: string;
@@ -21,7 +22,7 @@ export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
 
     const name = `${user.firstName} ${user.lastName}`;
 
-    async () => {
+    (async () => {
       try {
         const resp = await fetch(
           `/api/livekit?room=${chatId}&username=${name}`
@@ -31,8 +32,28 @@ export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
       } catch (e) {
         console.log(e);
       }
-    };
+    })();
   }, [user?.firstName, user?.lastName, chatId]);
 
-  return <div> Me</div>;
+  if (token === "") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <LiveKitRoom
+      data-lk-theme="default"
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      token={token}
+      connect={true}
+      video={video}
+      audio={audio}
+    >
+      <VideoConference />
+    </LiveKitRoom>
+  );
 };
